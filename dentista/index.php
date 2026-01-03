79f36e9959a03ce20623b6bee0a392dfa6764758
IgnacioLopezCalderon <lopezcalderonnacho06@gmail.com>
2026-01-03 16:11:00 +0100

    index.php

--- dentista/index.php
+++ dentista/index.php
@@ -7,28 +7,24 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Clases\Request;
 
 // iniciamos sesión
-// Clases\Sesion::init(); // Descomenta esto si ya tienes la clase Sesion
 
 // Recogemos las variables que nos manda el .htaccess
 // Si no hay nada, asumimos que estamos en Registro
 $controlador = Request::get('controlador') ?? 'Register';
 $metodo      = Request::get('metodo') ?? 'mostrar';
 
-// 3. Formamos el nombre completo de la clase
-// Ej: "Controladores\RegisterController"
+// Formamos el nombre completo de la clase
 $clase = "Controladores\\" . ucfirst($controlador) . "Controller";
 
-// 4. Cargamos el controlador
+// Cargamos el controlador
 if (class_exists($clase)) {
     $instancia = new $clase();
 
     if (method_exists($instancia, $metodo)) {
         $instancia->$metodo();
     } else {
-        // Fallback simple
         echo "Error 404: El método '$metodo' no existe en '$controlador'.";
     }
 } else {
-    // Fallback simple
     echo "Error 404: El controlador '$clase' no existe.";
 }
